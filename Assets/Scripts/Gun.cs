62a7a8cff91b2b033ba48c0b46c12c745a74beb9
Burak <Burakzgen@gmail.com>
2021-12-03 17:01:07 +0300

    Update
    Update

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,26 +3,39 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public float range, damage;
+    public float range, damage, fireRate, impactForce;
     public Camera fpsCam;
+    public ParticleSystem shootFlash;
+    private float nextTimeToFire = 0;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
     private void Shoot()
     {
+        shootFlash.Play();
         RaycastHit hitInfo;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, range))
         {
             //Debug.Log(hitInfo.transform.name);
             Target target = hitInfo.transform.GetComponent<Target>();
-            if (target!=null)
+            if (target != null)
             {
                 target.TakeDamage(damage);
+            }
+
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(-hitInfo.normal * impactForce);
+            }
+            if (hitInfo.collider.CompareTag("Target"))
+            {
+                hitInfo.transform.localScale += Vector3.one * 1.5f;
             }
         }
     }
